ee2121d8d39cfb31679c4354cba769a90c4a73c2
Jakob Englert <st155797@stud.uni-stuttgart.de>
2020-12-17 23:53:00 +0100

    Initial Commit

---
+++ esp8266/src/main.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include <esp8266wifi.h>
+#include <esp8266httpclient.h>
+
+#define RED D0
+#define GREEN D1
+#define BLUE D2
+#define SSID "RHEA"
+#define PASSSWORD "igkultur"
+
+
+void setup() {
+  pinMode(RED, OUTPUT);  
+  pinMode(GREEN, OUTPUT);  
+  pinMode(BLUE, OUTPUT);
+
+  Serial.begin(115200);
+ 
+  WiFi.begin(SSID, PASSSWORD);
+  Serial.print("Connecting..");
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    digitalWrite(BLUE, HIGH);
+    delay(100);
+    digitalWrite(BLUE, LOW);
+    delay(100);
+    Serial.print(".");
+    delay(500);
+  }
+  digitalWrite(D2, LOW);
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+ 
+    http.begin("http://192.168.137.1:5000/api/v1/tally/s/1");  //Specify request destination
+    int httpCode = http.GET();                                  //Send the request
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, LOW);
+    digitalWrite(BLUE, LOW);
+    
+    if(httpCode > 0) { //Check the returning code
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);             //Print the response payload
+      if(payload.equals("ready")){
+        digitalWrite(GREEN, HIGH);
+      } else if(payload.equals("live")){
+        digitalWrite(RED, HIGH);
+      } else {
+        digitalWrite(BLUE, HIGH);
+      }
+    }
+ 
+    http.end();   //Close connection
+ 
+  } else {
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, LOW);
+    digitalWrite(BLUE, HIGH);
+    delay(100);
+    digitalWrite(BLUE, LOW);
+    delay(100);
+  }
+ 
+  delay(200);
+}
