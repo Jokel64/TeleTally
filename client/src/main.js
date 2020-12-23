0fc3813d40eab633f15f45941a861af96ab077a1
Jakob Englert <st155797@stud.uni-stuttgart.de>
2020-12-23 01:22:39 +0100

    standaloneV0.1

--- client/src/main.js
+++ client/src/main.js
@@ -14,7 +14,7 @@ Vue.config.productionTip = false;
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'http://192.168.137.1:5000',
+  connection: 'http://127.0.0.1:5000',
   vuex: {
     store,
     actionPrefix: 'SOCKET_',
